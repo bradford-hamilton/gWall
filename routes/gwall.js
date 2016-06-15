4efa67088ac947a78162509feb3c5db720c85c79
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-14 22:17:49 -0600

    add timestamps, style, overflow:scroll in posts

--- routes/gwall.js
+++ routes/gwall.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('gwall');
+  knex.table('posts').select().then(function(posts) {
+    res.render('gwall', {posts: posts});
+  });
+});
+
+router.get('/post', function(request, response) {
+  response.render('post');
+});
+
+router.post('/post', function(request, response) {
+  // grab information from body
+  var username = request.body.username;
+  var image = request.body.image;
+  var rant = request.body.rant;
+  // insert into tables
+  knex.table('posts').insert({
+    username: username,
+    image: image,
+    rant: rant
+  }).then(function() {
+    // log to make sure we got here
+    console.log('You did it!');
+    response.redirect('/gwall');
+  }).catch(function(error) {
+    // catch error and if error direct to next middleware in app.js to send error page
+    next(error);
+  });
 });
 
 module.exports = router;
